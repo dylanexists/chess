e1cbf055dc96139a02d8627eddbe778e21e11283
Dylan Steenhoek <dylans18@byu.edu>
2026-01-17 14:39:11 -0700

    Simplified BishopMoveCalculator code into one for loop instead of two using an array to track directions.

--- shared/src/main/java/PieceCalculators/BishopMoveCalculator.java
+++ shared/src/main/java/PieceCalculators/BishopMoveCalculator.java
@@ -19,7 +19,30 @@ public class BishopMoveCalculator implements PieceMoveCalculator {
         int row_search_down = row - 1;
         boolean search_down_cond = true;
         ChessPosition test_pos = null;
+        int [] [] directions = { //possible directions of piece, {row, col}
+                {1, 1},
+                {-1, 1},
+                {1, -1},
+                {-1, -1}
+        };
 
+        for (int i = 0; i < directions.length; i++){ //for each possible piece direction
+            boolean search_cond = true;
+            int row_search = row + directions[i][0];
+            int col_search = col + directions[i][1];
+            while (search_cond && row_search >= 1 && row_search <= 8 && col_search >= 1 && col_search <= 8){ //while searching=true and possible move is on board
+                test_pos = new ChessPosition(row_search, col_search);
+                pathed_square_content = board.getPiece(test_pos); //get the content of the pathed square
+                if (pathed_square_content == null || pathed_square_content.getTeamColor() != bishop.getTeamColor()){ //if square is valid to move to
+                    moves.add(new ChessMove(pos, test_pos, null));
+                    row_search += directions[i][0];
+                    col_search += directions[i][1];
+                } else {search_cond = false;} //else, stop searching this direction
+            }
+        }
+
+        /*
+        //search right
         for (int c = col + 1; c <= 8; c++){
             if (row_search_up > 8){
                 search_up_cond = false;
@@ -45,6 +68,7 @@ public class BishopMoveCalculator implements PieceMoveCalculator {
             } else {search_down_cond = false;}}
         }
 
+        // search left
         pathed_square_content = null;
         row_search_up = row + 1;
         search_up_cond = true;
@@ -74,6 +98,8 @@ public class BishopMoveCalculator implements PieceMoveCalculator {
                 moves.add(new ChessMove(pos, test_pos, null));
             } else {search_down_cond = false;}}
         }
+        */
+
         return moves;
     }
 }
