4ba05361bcd9f8440d0a2306c741b88969b9db3b
Dylan Steenhoek <dylans18@byu.edu>
2026-01-17 14:48:06 -0700

    Fixed BishopMoveCalculator to account for enemy pieces.

--- shared/src/main/java/PieceCalculators/BishopMoveCalculator.java
+++ shared/src/main/java/PieceCalculators/BishopMoveCalculator.java
@@ -14,10 +14,6 @@ public class BishopMoveCalculator implements PieceMoveCalculator {
         int col = pos.getColumn();
         ChessPiece bishop = board.getPiece(pos);
         ChessPiece pathed_square_content = null;
-        int row_search_up = row + 1;
-        boolean search_up_cond = true;
-        int row_search_down = row - 1;
-        boolean search_down_cond = true;
         ChessPosition test_pos = null;
         int [] [] directions = { //possible directions of piece, {row, col}
                 {1, 1},
@@ -33,10 +29,13 @@ public class BishopMoveCalculator implements PieceMoveCalculator {
             while (search_cond && row_search >= 1 && row_search <= 8 && col_search >= 1 && col_search <= 8){ //while searching=true and possible move is on board
                 test_pos = new ChessPosition(row_search, col_search);
                 pathed_square_content = board.getPiece(test_pos); //get the content of the pathed square
-                if (pathed_square_content == null || pathed_square_content.getTeamColor() != bishop.getTeamColor()){ //if square is valid to move to
+                if (pathed_square_content == null){ //if square is empty, add move and continue searching
                     moves.add(new ChessMove(pos, test_pos, null));
                     row_search += directions[i][0];
                     col_search += directions[i][1];
+                } else if (pathed_square_content.getTeamColor() != bishop.getTeamColor()){ //elif square is an enemy, add move and stop searching
+                    moves.add(new ChessMove(pos, test_pos, null));
+                    search_cond = false;
                 } else {search_cond = false;} //else, stop searching this direction
             }
         }
