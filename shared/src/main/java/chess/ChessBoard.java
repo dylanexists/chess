16ea3b6617a121edde9a95de6fcc0f44f850e1af
Dylan Steenhoek <dylans18@byu.edu>
2026-02-15 01:46:38 -0700

    Completed "Getting Started" for Phase 3

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -11,7 +11,7 @@ import java.util.Objects;
  */
 public class ChessBoard implements Cloneable{
 
-    ChessPiece[][] grid = new ChessPiece[8][8];
+    private ChessPiece[][] grid = new ChessPiece[8][8];
     public ChessBoard() {
         
     }
