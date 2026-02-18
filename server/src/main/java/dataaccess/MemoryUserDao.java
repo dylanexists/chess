b3ba39ce1750a2f29a6003bd7dd7917e7e048cb8
Dylan Steenhoek <dylans18@byu.edu>
2026-02-17 23:12:29 -0700

    Implemented MemoryUserDao's method overrides.

--- server/src/main/java/dataaccess/MemoryUserDao.java
+++ server/src/main/java/dataaccess/MemoryUserDao.java
@@ -1,20 +1,34 @@
 package dataaccess;
 
+import java.util.HashMap;
+
 import model.UserData;
 
+
 public class MemoryUserDao implements UserDao{
+    final private HashMap<String, UserData> users = new HashMap<>();
+
     @Override
     public void clearUsers() throws QueryException {
-
+        users.clear();
     }
 
     @Override
     public UserData createUser(UserData u) throws DuplicateException {
-        return null;
+        if (users.containsKey(u.username())){
+            throw new DuplicateException("Username already exists");
+        }
+
+        users.put(u.username(), u);
+        return u;
     }
 
     @Override
     public UserData getUser(String username) throws NotFoundException {
-        return null;
+        UserData user = users.get(username);
+        if (user == null){
+            throw new NotFoundException("UserData not found");
+        }
+        return user;
     }
 }
