8a0814ae2e52fcd687f5317bf4d6b104ba0c8f4c
Dylan Steenhoek <dylans18@byu.edu>
2026-04-02 01:17:54 -0600

    Made ClientRepl implement ServerMessageObserver and pass itself to ServerFacade. Need to verify that this is the correct approach.

--- client/src/main/java/client/ClientRepl.java
+++ client/src/main/java/client/ClientRepl.java
@@ -2,20 +2,47 @@ package client;
 
 import chess.ChessGame;
 import client.facade.ServerFacade;
+import client.websocket.ServerMessageObserver;
 import facade.ResponseException;
+import ui.DrawnChessBoard;
+import websocket.messages.ErrorMessage;
+import websocket.messages.LoadGameMessage;
+import websocket.messages.NotificationMessage;
+import websocket.messages.ServerMessage;
 
-public class ClientRepl {
+import static ui.EscapeSequences.SET_TEXT_COLOR_RED;
+
+public class ClientRepl implements ServerMessageObserver {
     private final ServerFacade serverFac;
     private final PreLoginClient preLogin;
     private final PostLoginClient postLogin;
     private final InGameClient inGame;
 
     public ClientRepl(String serverUrl) throws ResponseException {
-        serverFac = new ServerFacade(serverUrl);
+        serverFac = new ServerFacade(serverUrl, this);
         preLogin = new PreLoginClient(serverFac);
         postLogin = new PostLoginClient(serverFac);
         inGame = new InGameClient(serverFac);
     }
+
+    @Override
+    public void notify(ServerMessage message) {
+        switch (message.getServerMessageType()) {
+            case NOTIFICATION -> displayNotification(((NotificationMessage) message).getMessage());
+            case ERROR -> displayError(((ErrorMessage) message).getErrorMessage());
+            case LOAD_GAME -> loadGame(((LoadGameMessage) message).getGame());
+        }
+    }
+
+    public void displayNotification(String notification) {
+        System.out.println(SET_TEXT_COLOR_RED + "Notif:" + notification);
+    }
+
+    public void displayError(String error) {
+        System.out.println(SET_TEXT_COLOR_RED + "Error:" + error);
+    }
+
+    public void loadGame(ChessGame game) {inGame.loadGame(game);}
 
     public void run() {
         ClientState state = ClientState.PRE_LOGIN;
