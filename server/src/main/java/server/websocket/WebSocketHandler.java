355da80f402d3338aec5bf769eaa2d5c1160c259
Dylan Steenhoek <dylans18@byu.edu>
2026-04-04 08:13:50 -0600

    Setup shell for WebSocketHandler

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,14 +1,28 @@
 package server.websocket;
 
+import chess.ChessMove;
 import com.google.gson.Gson;
+import dataaccess.AuthDao;
+import dataaccess.DataAccessException;
+import dataaccess.NotFoundException;
+import dataaccess.UserDao;
+import facade.ResponseException;
 import io.javalin.websocket.*;
+import model.AuthData;
+import org.eclipse.jetty.server.Authentication;
 import org.eclipse.jetty.websocket.api.Session;
 import org.jetbrains.annotations.NotNull;
+import websocket.commands.MakeMoveCommand;
 import websocket.commands.UserGameCommand;
 
 public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
     private final ConnectionManager connections = new ConnectionManager();
     private final Gson gson= new Gson();
+    private final AuthDao authDao;
+
+    public WebSocketHandler(AuthDao userDao) {
+        this.authDao = userDao;
+    }
 
     @Override
     public void handleConnect(WsConnectContext ctx) {
@@ -24,7 +38,15 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
         try {
             UserGameCommand command = serialize(wsMessageContext.message(), UserGameCommand.class);
             gameID = command.getGameID();
-            String authToken = command.getAuthToken();
+            String username = getUsername(command.getAuthToken());
+            saveSession(gameID, session);
+
+            switch (command.getCommandType()) {
+                case CONNECT -> connect(session, username, command);
+                case MAKE_MOVE -> makeMove(session, username, serialize(wsMessageContext.message(), MakeMoveCommand.class));
+                case LEAVE -> leave(session, username, command);
+                case RESIGN -> resign(session, username, command);
+            }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
@@ -37,5 +59,34 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
 
     private <T extends UserGameCommand> T serialize(String json, Class<T> givenClass) {
         return gson.fromJson(json, givenClass);
+    }
+
+    private void connect(Session session, String username, UserGameCommand command) {
+
+    }
+
+    private void makeMove(Session session, String username, MakeMoveCommand command) {
+
+    }
+
+    private void leave(Session session, String username, UserGameCommand command) {
+
+    }
+
+    private void resign(Session session, String username, UserGameCommand command) {
+
+    }
+
+    private void saveSession(Integer gameID, Session session) {
+        connections.add(gameID, session);
+    }
+
+    private String getUsername(String authToken) {
+        try {
+            AuthData auth = authDao.getAuth(authToken);
+            return auth.username();
+        } catch (DataAccessException ex) {
+            throw new ResponseException(ex.getMessage(), ex);
+        }
     }
 }
